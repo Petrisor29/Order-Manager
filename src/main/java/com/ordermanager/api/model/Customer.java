257a8be119644cf8cd67e58459798da6753445bb
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-06 00:32:27 +0200

    Update Customer.java

--- src/main/java/com/ordermanager/api/model/Customer.java
+++ src/main/java/com/ordermanager/api/model/Customer.java
@@ -1,18 +1,35 @@
 package com.ordermanager.api.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Customer {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private String email;
 
-    public Customer(int id, String name, String email){
-        this.id = id; this.name = name; this.email = email;
+    public Customer() {
     }
 
-    public int getId(){ return id; }
-    public String getName(){ return name; }
-    public String getEmail(){ return email; }
+    public Long getId() {
+        return id;
+    }
 
-    public void setName(String name){ this.name = name; }
-    public void setEmail(String email){ this.email = email; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) { this.email = email; }
 }
