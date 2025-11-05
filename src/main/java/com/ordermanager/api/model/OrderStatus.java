1222de43ea15867287e6c8a7245a39bf7bed56da
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-06 00:33:18 +0200

    Add PROCESSING status to OrderStatus enum

--- src/main/java/com/ordermanager/api/model/OrderStatus.java
+++ src/main/java/com/ordermanager/api/model/OrderStatus.java
@@ -1,2 +1,8 @@
 package com.ordermanager.api.model;
-public enum OrderStatus { NEW, PAID, SHIPPED, CANCELLED }
+
+public enum OrderStatus {
+    NEW,
+    PROCESSING,
+    SHIPPED,
+    CANCELLED
+}
