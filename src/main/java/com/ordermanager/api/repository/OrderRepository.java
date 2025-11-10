4f59bd58e787af3bba21114057758ea03345126f
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-10 21:40:31 +0200

    Add custom method to find orders by customer ID

--- src/main/java/com/ordermanager/api/repository/OrderRepository.java
+++ src/main/java/com/ordermanager/api/repository/OrderRepository.java
@@ -1,1 +1,11 @@
+package com.ordermanager.api.repository;
+
+import com.ordermanager.api.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    // poți adăuga metode custom
+    List<Order> findByCustomerId(Long customerId);
+}
 
