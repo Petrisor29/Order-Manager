66a044eec6678173092a9b249bb02ccd1d9bc6b4
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-10 21:35:51 +0200

    Create CustomerRepository interface for JPA

--- src/main/java/com/ordermanager/api/repository/CustomerRepository.java
+++ src/main/java/com/ordermanager/api/repository/CustomerRepository.java
@@ -1,1 +1,8 @@
+package com.ordermanager.api.repository;
+
+import com.ordermanager.api.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
 
