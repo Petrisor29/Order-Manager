48f8c9dc1ec022a1ad1a5dd701d896c660899f1e
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-10 22:48:35 +0200

    Add OrderItemRepository interface with findByOrderId method

--- src/main/java/com/ordermanager/api/repository/OrderItemRepository.java
+++ src/main/java/com/ordermanager/api/repository/OrderItemRepository.java
@@ -1,1 +1,10 @@
+package com.ordermanager.api.repository;
+
+import com.ordermanager.api.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findByOrderId(Long orderId);
+}
 
