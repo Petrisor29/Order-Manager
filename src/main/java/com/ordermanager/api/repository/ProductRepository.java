ce12ac0b7c07137a8aee5da6a400591b641416d6
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-10 21:39:59 +0200

    Add ProductRepository interface for Product entity

--- src/main/java/com/ordermanager/api/repository/ProductRepository.java
+++ src/main/java/com/ordermanager/api/repository/ProductRepository.java
@@ -1,1 +1,8 @@
+package com.ordermanager.api.repository;
+
+import com.ordermanager.api.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
 
