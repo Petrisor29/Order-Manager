854fc8b1fd34c0a157d5cd8c1394d72d33c15211
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-13 18:40:16 +0200

    Add ChangeStatusRequest class for order status updates

--- src/main/java/com/ordermanager/api/dto/ChangeStatusRequest.java
+++ src/main/java/com/ordermanager/api/dto/ChangeStatusRequest.java
@@ -1,1 +1,20 @@
+package com.ordermanager.api.dto;
+
+import com.ordermanager.api.model.OrderStatus;
+
+public class ChangeStatusRequest {
+
+    private OrderStatus status;
+
+    public ChangeStatusRequest() {
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
 
