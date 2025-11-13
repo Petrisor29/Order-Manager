01da40550874bee9c848e8077b80cce8af0e28b6
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-13 18:31:57 +0200

    Implement CustomerId in CreateOrderRequest DTO
    Add CustomerId field with getter and setter methods.

--- src/main/java/com/ordermanager/api/dto/CreateOrderRequest.java
+++ src/main/java/com/ordermanager/api/dto/CreateOrderRequest.java
@@ -1,1 +1,15 @@
+package com.ordermanager.api.dto;
 
+public class CreateOrderRequest{
+  private Long CustomerId;
+
+  public CreateOrderRequest(){
+  }
+
+  public Long getCustomerId(){
+    return customerId;
+  }
+
+  public void setCustomerId(){
+    this.customerId = customerId;
+  
