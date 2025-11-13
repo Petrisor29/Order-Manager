99be182510999f18daa54e0b9f2d9411c75ac8cc
Petrisor29 <petrisor.gnr@gmail.com>
2025-11-13 18:39:03 +0200

    Create AddItemRequest DTO with productId and quantity
    Added fields for product ID and quantity with getters and setters.

--- src/main/java/com/ordermanager/api/dto/AddItemRequest.java
+++ src/main/java/com/ordermanager/api/dto/AddItemRequest.java
@@ -1,1 +1,26 @@
+package com.ordermanager.api.dto;
 
+public class AddItemRequest{
+
+  private Long productId;
+  private Integer quantity;
+  public AddItemRequest(){
+  }
+
+  public Long getProductId(){
+    return productId;
+  }
+
+  public void setProductId(Long productId){
+    this.productId = productId;
+  }
+
+  public Integer getQuantity(){
+    return quantity;
+  }
+
+  public void setQuantity(){
+    this.quantity = quantity;
+  }
+
+}
